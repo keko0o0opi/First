81a1ef09990cd04a762990c94f33fbbbfd1e11cd
keko0o0opi <144677926+keko0o0opi@users.noreply.github.com>
2023-09-11 20:32:03 +0800

    Add files via upload

---
+++ FrmRegistration/FrmConfirm/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmConfirm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DelProgram = new DelegateText();
+        }
+  
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private DelegateText DelProgram, DelLastName, DelFirstName, DelMiddleName,
+    }
+}
---
+++ FrmRegistration/FrmRegistration/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmRegistration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StudentInfoClass.FirstName = textBox4.Text.ToString();
+            StudentInfoClass.LastName = textBox3.Text.ToString();
+            StudentInfoClass.MiddleName = textBox5.Text.ToString();
+            StudentInfoClass.Address = textBox8.Text.ToString();
+            StudentInfoClass.Program = comboBox1.Text.ToString();
+            StudentInfoClass.Age = Convert.ToInt64(textBox6.Text.ToString());
+            StudentInfoClass.ContactNo = Convert.ToInt64(textBox7.Text.ToString());
+            StudentInfoClass.StudentNo = Convert.ToInt64(textBox1.Text.ToString());
+
+        }
+    }
+}
